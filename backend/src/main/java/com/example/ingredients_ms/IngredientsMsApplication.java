383ebfc531a4ce0a415284085b94c69fb81adaf2
jerry6475 <jerry6475@naver.com>
2025-06-11 17:18:06 +0900

    Initial commit

---
+++ backend/src/main/java/com/example/ingredients_ms/IngredientsMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.ingredients_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngredientsMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IngredientsMsApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/example/ingredients_ms/IngredientsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ingredients_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngredientsMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
