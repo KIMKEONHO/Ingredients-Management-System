968df09bf432d65f013fc5e77f19bf431ed04342
jerry6475 <jerry6475@naver.com>
2025-06-30 12:58:55 +0900

    무중단 배포

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -29,9 +29,13 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security") // security
     implementation("org.springframework.boot:spring-boot-starter-validation")
-    implementation("org.springframework.boot:spring-boot-starter-data-redis")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-mail")
+
+    // redis
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.session:spring-session-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
 
     // Lombok
     compileOnly("org.projectlombok:lombok")
@@ -81,7 +85,6 @@ dependencies {
     // STOMP
 //    implementation("org.springframework.boot:spring-boot-starter-websocket")
 //    implementation("org.springframework.boot:spring-boot-starter-webflux")
-//    implementation("org.springframework.boot:spring-boot-starter-actuator")
 //    implementation("org.springframework.boot:spring-boot-starter-aop")
 //    implementation("org.springframework.boot:spring-boot-starter-cache")
 
