83813962a073b0713958a8a9fabad86f7f8cc9bc
KIMKEONHO <jerry6475@naver.com>
2025-08-18 16:09:20 +0900

    [ chore ] : #54 유통기한 상태 추가

---
+++ backend/src/main/java/com/example/ingredients_ms/domain/foodinventory/controller/FoodInventoryStatisticsController.java
@@ -0,0 +1,13 @@
+package com.example.ingredients_ms.domain.foodinventory.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/inventory/statistics")
+public class FoodInventoryStatisticsController {
+
+
+
+
+}
