7cb566215b4499f123feef4ec5e76a51ea2514cc
KIMKEONHO <jerry6475@naver.com>
2025-08-19 15:37:45 +0900

    [ feat ] : #54 가장 많은 소비량을 가지고 있는 식재료 결과 반환을 위한 service 로직 추가

--- backend/src/main/java/com/example/ingredients_ms/domain/foodinventory/service/FoodInventoryStatisticsService.java
+++ backend/src/main/java/com/example/ingredients_ms/domain/foodinventory/service/FoodInventoryStatisticsService.java
@@ -1,12 +1,15 @@
 package com.example.ingredients_ms.domain.foodinventory.service;
 
+import com.example.ingredients_ms.domain.foodinventory.dto.response.ConsumedRankResponseDto;
 import com.example.ingredients_ms.domain.foodinventory.dto.response.WasteStatisticsResponseDto;
 import com.example.ingredients_ms.domain.foodinventory.repository.FoodInventoryRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 @Service
@@ -40,5 +43,27 @@ public class FoodInventoryStatisticsService {
                 })
                 .collect(Collectors.toList());
 
+    }
+
+    @Transactional
+    public List<ConsumedRankResponseDto> mostConsumedStatistics(long userId) {
+
+        List<FoodInventoryRepository.IngredientStatsProjection> inventories =
+                foodInventoryRepository.findTopConsumedByUserId(userId, PageRequest.of(0, 5));
+
+        AtomicInteger rankCounter = new AtomicInteger(1); // 순위 카운터
+
+        return inventories.stream()
+                .map(p -> {
+                    long consumedQty = p.getConsumedQty() != null ? p.getConsumedQty() : 0L;
+
+                    return ConsumedRankResponseDto.builder()
+                            .rank(rankCounter.getAndIncrement()) // 순위 부여
+                            .ingredientName(p.getIngredientName())
+                            .ingredientId(p.getIngredientId())
+                            .consumedCount(consumedQty)
+                            .build();
+                })
+                .collect(Collectors.toList());
     }
 }
