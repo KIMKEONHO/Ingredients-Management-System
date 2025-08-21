f2e36f3d13ea1f429a8940319a3ae0b073f7fe8f
KIMKEONHO <jerry6475@naver.com>
2025-08-21 14:16:22 +0900

    [ fix ] : #61 주석으로 의미 남기기

--- backend/src/main/java/com/example/ingredients_ms/domain/complaint/entity/ComplaintStatus.java
+++ backend/src/main/java/com/example/ingredients_ms/domain/complaint/entity/ComplaintStatus.java
@@ -2,6 +2,8 @@ package com.example.ingredients_ms.domain.complaint.entity;
 
 // Complaint.java 내부 또는 별도 파일
 public enum ComplaintStatus {
+
+    // 보류, 진행 중, 완료, 거부됨
     PENDING(1),
     IN_PROGRESS(2),
     COMPLETED(3),
