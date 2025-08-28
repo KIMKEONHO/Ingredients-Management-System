161570c082c007e20e3d70a1c4054c5b471e0d97
realize2578 <realize2578@gmail.com>
2025-08-28 15:06:36 +0900

    [ chore ] : #82 백엔드 식품 재고 수량 수정 dto 추가

---
+++ backend/src/main/java/com/example/ingredients_ms/domain/foodinventory/dto/request/UpdateFoodInventoryQuantityRequestDto.java
@@ -0,0 +1,10 @@
+package com.example.ingredients_ms.domain.foodinventory.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateFoodInventoryQuantityRequestDto {
+    private int quantity;
+}
