224f6d7d113eb7ed8b6c383e20d68baadbb41b0e
KIMKEONHO <jerry6475@naver.com>
2025-09-02 15:12:02 +0900

    [ feat ] : #90 [관리자] 유저 상태 변경, 유저 삭제 repository 추가

--- backend/src/main/java/com/example/ingredients_ms/domain/user/repository/UserRepository.java
+++ backend/src/main/java/com/example/ingredients_ms/domain/user/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package com.example.ingredients_ms.domain.user.repository;
 
+import com.example.ingredients_ms.domain.user.entity.Role;
 import com.example.ingredients_ms.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -10,4 +12,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByRefreshToken(String refreshToken);
     Optional<User> findBySocialIdAndSsoProvider(String socialId, String socialProvider);
     Optional<User> findByPhoneNumAndUserName(String phoneNum, String userName);
+    List<User> findByRole(Role role);
 }
