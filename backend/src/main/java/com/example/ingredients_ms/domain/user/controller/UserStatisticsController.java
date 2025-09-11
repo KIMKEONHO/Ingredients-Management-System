fb6f791bba3fdd0e2b1ee60464aa20bff27ee3fd
KIMKEONHO <jerry6475@naver.com>
2025-09-11 14:18:32 +0900

    [ feat ] : #97 시간별 유저 통계 기능 컨트롤러 기능 추가

--- backend/src/main/java/com/example/ingredients_ms/domain/user/controller/UserStatisticsController.java
+++ backend/src/main/java/com/example/ingredients_ms/domain/user/controller/UserStatisticsController.java
@@ -1,10 +1,12 @@
 package com.example.ingredients_ms.domain.user.controller;
 
 import com.example.ingredients_ms.domain.user.dto.response.AllUserStatisticsResponseDto;
+import com.example.ingredients_ms.domain.user.dto.response.UserStatisticsResponseDto;
 import com.example.ingredients_ms.domain.user.service.UserStatisticsService;
 import com.example.ingredients_ms.global.rsdata.RsData;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +25,11 @@ public class UserStatisticsController {
         List<AllUserStatisticsResponseDto> response = userStatisticsService.getAllUser();
 
         return new RsData<>("200", "모든 유저의 정보를 찾았습니다.", response);
+    }
+
+    @GetMapping("/{theme}")
+    public RsData<UserStatisticsResponseDto> getUserStatistics(@PathVariable String theme) {
+        UserStatisticsResponseDto response = userStatisticsService.getStatistics(theme);
+        return new RsData<>("200", theme + " 단위의 유저 통계입니다.", response);
     }
 }
