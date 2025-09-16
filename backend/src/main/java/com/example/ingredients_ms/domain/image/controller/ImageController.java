c8c30d9c870e77ee26526a3b79e565a469c21405
KIMKEONHO <jerry6475@naver.com>
2025-09-16 14:54:53 +0900

    [ refact ] : #103 이미지 컨트롤러 서비스 분할

--- backend/src/main/java/com/example/ingredients_ms/domain/image/controller/ImageController.java
+++ backend/src/main/java/com/example/ingredients_ms/domain/image/controller/ImageController.java
@@ -1,67 +1,31 @@
 package com.example.ingredients_ms.domain.image.controller;
 
+import com.example.ingredients_ms.domain.image.dto.ImageUploadResponseDto;
+import com.example.ingredients_ms.domain.image.service.ImageService;
 import com.example.ingredients_ms.global.rsdata.RsData;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import software.amazon.awssdk.core.sync.RequestBody;
-import software.amazon.awssdk.services.s3.S3Client;
-import software.amazon.awssdk.services.s3.model.Bucket;
-import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/images")
-public class ImageController{
+@RequestMapping("/api/v1/images")
+@Tag(name = "이미지 관리 API", description = "이미지 업로드 및 삭제를 위한 API")
+public class ImageController {
 
-    private final S3Client s3Client;
+    private final ImageService imageService;
 
-    private static final String BUCKET_NAME = "dev-bucket-lolgun0629-1";
-    private static final String REGION = "ap-northeast-2";
-    private static final String IMG_DIR_NAME = "img1";
-
-    public static String getS3FileUrl(String fileName) {
-        return "https://" + BUCKET_NAME + ".s3." + REGION + ".amazonaws.com/" + fileName;
-    }
-
-    @GetMapping("/")
-    public List<String> home() {
-        List<Bucket> bucketList = s3Client.listBuckets().buckets();
-        return bucketList.stream().map(Bucket::name).collect(Collectors.toList());
-    }
-
-    // 이미지 업로드
+    @Operation(summary = "이미지 업로드", description = "이미지 파일을 S3에 업로드합니다.")
     @PostMapping("/upload")
-    @ResponseBody
-    public RsData<?> handleFileUpload(MultipartFile file) throws IOException {
-        PutObjectRequest putObjectRequest = PutObjectRequest.builder()
-                .bucket(BUCKET_NAME)
-                .key(IMG_DIR_NAME + "/" + file.getOriginalFilename())
-                .contentType(file.getContentType())
-                .build();
-
-        s3Client.putObject(putObjectRequest,
-                RequestBody.fromInputStream(file.getInputStream(), file.getSize()));
-
-        return new RsData<>("201","이미지가 추가되었습니다.");
+    public RsData<ImageUploadResponseDto> uploadImage(@RequestParam("file") MultipartFile file) {
+        return imageService.uploadImage(file);
     }
 
-    // 이미지 삭제
-    @PostMapping("/deleteFile")
-    @ResponseBody
-    public RsData<?> deleteFile(String fileName) {
-        DeleteObjectRequest deleteObjectRequest = DeleteObjectRequest.builder()
-                .bucket(BUCKET_NAME)
-                .key(IMG_DIR_NAME + "/" + fileName)
-                .build();
-
-        s3Client.deleteObject(deleteObjectRequest);
-        return new RsData<>("204","이미지가 삭제되었습니다.");
+    @Operation(summary = "이미지 삭제", description = "S3에서 이미지를 삭제합니다.")
+    @DeleteMapping("/{fileName}")
+    public RsData<Void> deleteImage(@PathVariable String fileName) {
+        return imageService.deleteImage(fileName);
     }
-
 }
