1689b51c1c962d9522aa54a47457555fc56c2f07
KIMKEONHO <jerry6475@naver.com>
2025-09-23 15:31:52 +0900

    [ add ] : #114 레시피 재료 컨트롤러 추가

---
+++ backend/src/main/java/com/example/ingredients_ms/domain/recipeingredient/controller/RecipeIngredientController.java
@@ -0,0 +1,11 @@
+package com.example.ingredients_ms.domain.recipeingredient.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/recipe/ingredient")
+@RequiredArgsConstructor
+public class RecipeIngredientController {
+}
