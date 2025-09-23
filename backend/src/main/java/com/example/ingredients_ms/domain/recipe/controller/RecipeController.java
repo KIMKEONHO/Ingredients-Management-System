ab6142f62e6fcb58aac4d1443d780ec78af80169
KIMKEONHO <jerry6475@naver.com>
2025-09-23 15:11:38 +0900

    [ add ] : #114 레시피 컨트롤러 추가

---
+++ backend/src/main/java/com/example/ingredients_ms/domain/recipe/controller/RecipeController.java
@@ -0,0 +1,32 @@
+package com.example.ingredients_ms.domain.recipe.controller;
+
+import com.example.ingredients_ms.domain.recipe.dto.request.CreateRecipeRequestDto;
+import com.example.ingredients_ms.domain.recipe.service.RecipeService;
+import com.example.ingredients_ms.global.rsdata.RsData;
+import com.example.ingredients_ms.global.security.CurrentUser;
+import com.example.ingredients_ms.global.security.SecurityUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/recipe")
+@RequiredArgsConstructor
+public class RecipeController {
+
+    private final RecipeService recipeService;
+
+    @PostMapping("/")
+    public RsData<?> createRecipe(
+            @CurrentUser SecurityUser securityUser,
+            @RequestBody CreateRecipeRequestDto requestDto
+            ){
+
+        return recipeService.createService(securityUser.getId(), requestDto);
+    }
+
+//    @GetMapping("/all")
+//    public RsData<?> findAllRecipe(){
+//
+//        return recipeService.findAllRecipe();
+//    }
+}
