2e97b629a6a2e5db097090adc598d74040bd48a5
KIMKEONHO <jerry6475@naver.com>
2025-09-24 15:20:26 +0900

    [ feat ] : #114 레시피 재료 생성 로직 작성

--- backend/src/main/java/com/example/ingredients_ms/domain/recipeingredient/service/RecipeIngredientService.java
+++ backend/src/main/java/com/example/ingredients_ms/domain/recipeingredient/service/RecipeIngredientService.java
@@ -1,7 +1,38 @@
 package com.example.ingredients_ms.domain.recipeingredient.service;
 
+import com.example.ingredients_ms.domain.ingredients.entity.Ingredients;
+import com.example.ingredients_ms.domain.ingredients.service.IngredientsService;
+import com.example.ingredients_ms.domain.recipe.entity.Recipe;
+import com.example.ingredients_ms.domain.recipeingredient.dto.request.CreateRecipeIngredientsRequestDto;
+import com.example.ingredients_ms.domain.recipeingredient.entity.RecipeIngredient;
+import com.example.ingredients_ms.domain.recipeingredient.repository.RecipeIngredientRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class RecipeIngredientService {
+
+    private final RecipeIngredientRepository recipeIngredientRepository;
+    private final IngredientsService ingredientsService;
+
+    public void createRecipeIngredient(List<CreateRecipeIngredientsRequestDto> requestDto, Recipe recipe){
+
+        for(CreateRecipeIngredientsRequestDto ingredientDto : requestDto) {
+
+            Ingredients ingredients = ingredientsService.findById(ingredientDto.getIngredientId());
+
+            RecipeIngredient recipeIngredient = RecipeIngredient.builder()
+                    .recipe(recipe)
+                    .ingredient(ingredients)
+                    .notes(ingredientDto.getNotes().isEmpty() ? null : ingredientDto.getNotes())
+                    .unit(ingredientDto.getUnit())
+                    .quantity(ingredientDto.getQuantity())
+                .build();
+
+            recipeIngredientRepository.save(recipeIngredient);
+        }
+    }
 }
