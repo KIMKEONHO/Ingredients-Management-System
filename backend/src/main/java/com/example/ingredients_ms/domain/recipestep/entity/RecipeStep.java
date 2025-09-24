cc04752c03bf829c57619f3123c70e548470bdb5
KIMKEONHO <jerry6475@naver.com>
2025-09-24 15:20:59 +0900

    [ feat ] : #114 레시피 단계 엔티티 SuperBuilder 추가

--- backend/src/main/java/com/example/ingredients_ms/domain/recipestep/entity/RecipeStep.java
+++ backend/src/main/java/com/example/ingredients_ms/domain/recipestep/entity/RecipeStep.java
@@ -3,9 +3,11 @@ package com.example.ingredients_ms.domain.recipestep.entity;
 import com.example.ingredients_ms.domain.recipe.entity.Recipe;
 import com.example.ingredients_ms.global.entity.BaseEntity;
 import jakarta.persistence.*;
+import lombok.experimental.SuperBuilder;
 
 @Entity
 @Table(name = "recipe_step")
+@SuperBuilder
 public class RecipeStep extends BaseEntity {
     @Column(name = "step_number", nullable = false)
     private Integer stepNumber;
