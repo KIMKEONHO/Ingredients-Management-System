40b83742e57d416ffdd3559df19e7d78f7b3b2a2
KIMKEONHO <jerry6475@naver.com>
2025-09-24 14:26:05 +0900

    [ add ] : #114 레시피 재료 dto 추가

---
+++ backend/src/main/java/com/example/ingredients_ms/domain/recipeingredient/dto/request/CreateRecipeIngredientsRequestDto.java
@@ -0,0 +1,11 @@
+package com.example.ingredients_ms.domain.recipeingredient.dto.request;
+
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+@Getter
+public class CreateRecipeIngredientsRequestDto {
+
+    private Long ingredientId;
+}
