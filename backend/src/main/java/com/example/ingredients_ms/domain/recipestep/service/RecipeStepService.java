2b34bb87797bd05e8ca5a7f93e2ee8875dc02331
KIMKEONHO <jerry6475@naver.com>
2025-09-24 15:21:58 +0900

    [ feat ] : #114 레시피 단계 서비스 단계 생성 로직 추가

--- backend/src/main/java/com/example/ingredients_ms/domain/recipestep/service/RecipeStepService.java
+++ backend/src/main/java/com/example/ingredients_ms/domain/recipestep/service/RecipeStepService.java
@@ -1,4 +1,37 @@
 package com.example.ingredients_ms.domain.recipestep.service;
 
+import com.example.ingredients_ms.domain.recipe.entity.Recipe;
+import com.example.ingredients_ms.domain.recipestep.dto.request.CreateRecipeStepRequestDto;
+import com.example.ingredients_ms.domain.recipestep.entity.RecipeStep;
+import com.example.ingredients_ms.domain.recipestep.repository.RecipeStepRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class RecipeStepService {
+
+    private final RecipeStepRepository recipeStepRepository;
+
+    public void createRecipeStep(List<CreateRecipeStepRequestDto> requestDto, Recipe recipe){
+
+        for(CreateRecipeStepRequestDto recipeStepRequestDto : requestDto){
+
+            RecipeStep recipeStep = RecipeStep.builder()
+                    .stepNumber(recipeStepRequestDto.getStepNumber())
+                    .description(recipeStepRequestDto.getDescription())
+                    .cookingTime(recipeStepRequestDto.getCookingTime())
+                    .imageUrl(recipeStepRequestDto.getImageUrl())
+                    .recipe(recipe)
+                    .build();
+
+            recipeStepRepository.save(recipeStep);
+        }
+
+
+    }
+
+
 }
