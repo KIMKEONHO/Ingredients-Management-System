f4ae197290da968a4780a532933ff186f4e7da0f
KIMKEONHO <jerry6475@naver.com>
2025-09-24 15:19:55 +0900

    [ chore ] : #114 레시피 재료 컨트롤러 작성

--- backend/src/main/java/com/example/ingredients_ms/domain/recipeingredient/controller/RecipeIngredientController.java
+++ backend/src/main/java/com/example/ingredients_ms/domain/recipeingredient/controller/RecipeIngredientController.java
@@ -1,5 +1,6 @@
 package com.example.ingredients_ms.domain.recipeingredient.controller;
 
+import com.example.ingredients_ms.domain.recipeingredient.service.RecipeIngredientService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/recipe/ingredient")
 @RequiredArgsConstructor
 public class RecipeIngredientController {
+
+    private final RecipeIngredientService recipeIngredientService;
+
 }
