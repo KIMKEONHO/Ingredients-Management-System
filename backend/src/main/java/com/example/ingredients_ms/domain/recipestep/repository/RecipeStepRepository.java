ceeeeea844b66613abcd9ac3f960a84bbb13e0fe
KIMKEONHO <jerry6475@naver.com>
2025-09-24 15:21:37 +0900

    [ chore ] : #114 레시피 단계 리파지토리 @Repository 어노테이션 누락 추가

--- backend/src/main/java/com/example/ingredients_ms/domain/recipestep/repository/RecipeStepRepository.java
+++ backend/src/main/java/com/example/ingredients_ms/domain/recipestep/repository/RecipeStepRepository.java
@@ -2,6 +2,8 @@ package com.example.ingredients_ms.domain.recipestep.repository;
 
 import com.example.ingredients_ms.domain.recipestep.entity.RecipeStep;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RecipeStepRepository extends JpaRepository<RecipeStep,Long> {
 }
