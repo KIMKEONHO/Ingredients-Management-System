c2cc40ec815c040f91e812e2b661f0eab1029f16
KIMKEONHO <jerry6475@naver.com>
2025-09-24 16:21:28 +0900

    [ feat ] : #114 이미지를 입력받게 수정

--- backend/src/main/java/com/example/ingredients_ms/domain/recipe/controller/RecipeController.java
+++ backend/src/main/java/com/example/ingredients_ms/domain/recipe/controller/RecipeController.java
@@ -6,7 +6,14 @@ import com.example.ingredients_ms.global.rsdata.RsData;
 import com.example.ingredients_ms.global.security.CurrentUser;
 import com.example.ingredients_ms.global.security.SecurityUser;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/recipe")
@@ -15,13 +22,14 @@ public class RecipeController {
 
     private final RecipeService recipeService;
 
-    @PostMapping("/")
+    @PostMapping(value = "/", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public RsData<?> createRecipe(
             @CurrentUser SecurityUser securityUser,
-            @RequestBody CreateRecipeRequestDto requestDto
-            ){
-
-        return recipeService.createRecipePost(securityUser.getId(), requestDto);
+            @RequestPart("recipeData") CreateRecipeRequestDto requestDto,
+            @RequestPart(value = "recipeImage", required = false) MultipartFile recipeImage,
+            @RequestPart(value = "stepImages", required = false) List<MultipartFile> stepImages
+    ) {
+        return recipeService.createRecipePost(securityUser.getId(), requestDto, recipeImage, stepImages);
     }
 
 //    @GetMapping("/all")
