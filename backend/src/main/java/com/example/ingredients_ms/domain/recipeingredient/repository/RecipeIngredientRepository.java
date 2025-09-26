7335a4f0a4da3b4e6b9a96cd38355723098c7e4a
KIMKEONHO <jerry6475@naver.com>
2025-09-26 16:06:20 +0900

    [ feat ] : #114 모든 식단 찾는 로직 리파지토리에 추가

--- backend/src/main/java/com/example/ingredients_ms/domain/recipeingredient/repository/RecipeIngredientRepository.java
+++ backend/src/main/java/com/example/ingredients_ms/domain/recipeingredient/repository/RecipeIngredientRepository.java
@@ -4,6 +4,10 @@ import com.example.ingredients_ms.domain.recipeingredient.entity.RecipeIngredien
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RecipeIngredientRepository extends JpaRepository<RecipeIngredient, Long> {
+
+    List<RecipeIngredient> findByRecipeId(Long recipeId);
 }
