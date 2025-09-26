39b2528ca57f0553687af89981319d4ddcc12633
KIMKEONHO <jerry6475@naver.com>
2025-09-26 16:06:29 +0900

    [ feat ] : #114 dto 빌더 추가

--- backend/src/main/java/com/example/ingredients_ms/domain/recipeingredient/dto/response/RecipeIngredientResponseDto.java
+++ backend/src/main/java/com/example/ingredients_ms/domain/recipeingredient/dto/response/RecipeIngredientResponseDto.java
@@ -1,5 +1,6 @@
 package com.example.ingredients_ms.domain.recipeingredient.dto.response;
 
+import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,6 +8,7 @@ import lombok.Setter;
 @Data
 @Getter
 @Setter
+@Builder
 public class RecipeIngredientResponseDto {
 
     private String ingredientName;
