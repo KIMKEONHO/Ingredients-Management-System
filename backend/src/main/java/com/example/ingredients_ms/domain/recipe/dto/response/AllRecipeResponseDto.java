dd82da2c7fa177d382801c7828ac098593068b97
KIMKEONHO <jerry6475@naver.com>
2025-09-26 15:30:37 +0900

    [ add ] : #114 전체 레시피 dto 추가

---
+++ backend/src/main/java/com/example/ingredients_ms/domain/recipe/dto/response/AllRecipeResponseDto.java
@@ -0,0 +1,31 @@
+package com.example.ingredients_ms.domain.recipe.dto.response;
+
+import com.example.ingredients_ms.domain.recipeingredient.dto.response.RecipeIngredientResponseDto;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Getter
+@Setter
+public class AllRecipeResponseDto {
+
+    // 레시피 테이블에서 가져올 정보들
+    private String title;
+    private String userNickName;
+    private String userProfile;
+    private String description;
+    private LocalDateTime createdAt;
+    private Integer difficultyLevel;
+    private Integer cookingTime;
+
+    // 레시피 재료
+    private List<RecipeIngredientResponseDto> recipeIngredientResponseDto;
+
+    // 좋아요 갯수
+    private Integer likeCount;
+
+}
