83d6c7c785c5841cf8150093f24c9a685724b396
KIMKEONHO <jerry6475@naver.com>
2025-09-26 16:03:42 +0900

    [ chore ] : #114 dto 빌더 추가

--- backend/src/main/java/com/example/ingredients_ms/domain/recipe/dto/response/AllRecipeResponseDto.java
+++ backend/src/main/java/com/example/ingredients_ms/domain/recipe/dto/response/AllRecipeResponseDto.java
@@ -1,6 +1,7 @@
 package com.example.ingredients_ms.domain.recipe.dto.response;
 
 import com.example.ingredients_ms.domain.recipeingredient.dto.response.RecipeIngredientResponseDto;
+import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,6 +12,7 @@ import java.util.List;
 @Data
 @Getter
 @Setter
+@Builder
 public class AllRecipeResponseDto {
 
     // 레시피 테이블에서 가져올 정보들
