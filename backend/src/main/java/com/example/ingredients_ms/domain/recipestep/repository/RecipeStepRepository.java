dbf8c0fb106ded70708d00a6792c44e1527c4a38
KIMKEONHO <jerry6475@naver.com>
2025-09-29 14:17:58 +0900

    [ feat ] : #117 레시피 Id를 통해 레시피 단계 찾기 로직 추가

--- backend/src/main/java/com/example/ingredients_ms/domain/recipestep/repository/RecipeStepRepository.java
+++ backend/src/main/java/com/example/ingredients_ms/domain/recipestep/repository/RecipeStepRepository.java
@@ -4,6 +4,10 @@ import com.example.ingredients_ms.domain.recipestep.entity.RecipeStep;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface RecipeStepRepository extends JpaRepository<RecipeStep,Long> {
+
+    List<RecipeStep> findByRecipeId(Long recipeId);
 }
