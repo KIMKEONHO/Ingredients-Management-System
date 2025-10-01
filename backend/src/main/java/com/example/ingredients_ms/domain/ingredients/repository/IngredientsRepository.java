106f5e49526906e83ba0b63d2556434b7553d602
KIMKEONHO <jerry6475@naver.com>
2025-10-01 16:05:02 +0900

    [ feat ] : #121 재료 통계용 리파지토리 로직 추가

--- backend/src/main/java/com/example/ingredients_ms/domain/ingredients/repository/IngredientsRepository.java
+++ backend/src/main/java/com/example/ingredients_ms/domain/ingredients/repository/IngredientsRepository.java
@@ -4,6 +4,7 @@ import com.example.ingredients_ms.domain.ingredients.entity.Ingredients;
 import com.example.ingredients_ms.domain.ingredientscategory.entity.IngredientsCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,5 +20,8 @@ public interface IngredientsRepository extends JpaRepository<Ingredients, Long> 
     Ingredients findById(long id);
 
     Optional<Ingredients> findByName(String name);
+
+    // 통계용 메서드
+    long countByCreatedAtBetween(LocalDateTime start, LocalDateTime end);
 
 }
