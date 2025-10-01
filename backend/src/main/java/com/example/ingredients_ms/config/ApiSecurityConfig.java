127d3216c94503302383ba73259339227cf33d37
KIMKEONHO <jerry6475@naver.com>
2025-10-01 14:56:30 +0900

    [ feat ] : #121 유저 정보 변경 url 추가

--- backend/src/main/java/com/example/ingredients_ms/config/ApiSecurityConfig.java
+++ backend/src/main/java/com/example/ingredients_ms/config/ApiSecurityConfig.java
@@ -89,6 +89,8 @@ public class ApiSecurityConfig {
                         .requestMatchers(HttpMethod.DELETE,"/api/*/users/drop/*").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PATCH,"/api/*/users/change/userdata").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.POST,"/api/*/users/exchange/profile").hasAnyRole("USER","ADMIN")
+                        .requestMatchers(HttpMethod.PATCH, "/api/*/users/change/userdata").hasAnyRole("USER","ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/api/*/users/exchange/profile-data").hasAnyRole("USER","ADMIN")
 
                         // 유저 통계 및 관리 api
                         .requestMatchers(HttpMethod.GET, "/api/*/users/statistics/").hasRole("ADMIN")
