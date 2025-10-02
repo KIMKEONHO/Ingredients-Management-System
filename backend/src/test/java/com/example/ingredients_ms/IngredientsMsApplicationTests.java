f17b5000946b4b45f767f40886f7e74b4226c9ab
KIMKEONHO <jerry6475@naver.com>
2025-10-02 17:39:55 +0900

    [ fix ] : #132 V1 배포 테스트

--- backend/src/test/java/com/example/ingredients_ms/IngredientsMsApplicationTests.java
+++ backend/src/test/java/com/example/ingredients_ms/IngredientsMsApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.ingredients_ms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class IngredientsMsApplicationTests {
 
     @Test
