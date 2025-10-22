b97f2f06dd7ca660c536e1aeaa15d2ac1794286a
KIMKEONHO <jerry6475@naver.com>
2025-10-22 21:40:19 +0900

    [ fix ] : #132 자동화 배포 수정

---
+++ backend/src/main/java/com/example/ingredients_ms/global/app/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.ingredients_ms.global.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String showMain(){return "Hello world!!!";}
+
+}
